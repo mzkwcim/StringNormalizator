fe0143889fa9be1407622677bae5ca97dd1c48d2
M5kwc1m <144521687+mzkwcim@users.noreply.github.com>
2024-03-03 22:17:31 +0100

    Update DocumentToSpreedSheetGetter.js

--- DocumentToSpreedSheetGetter.js
+++ DocumentToSpreedSheetGetter.js
@@ -1,26 +1,18 @@
 function kopiujTekstZDokumentuDoArkusza() {
-  // ID dokumentu Google Docs
   var dokumentId = '1dWTY8Z5suin4BUPBVRAYJWDZxuS-g70GxkUxAKbvH80';
 
-  // Otwórz dokument
   var dokument = DocumentApp.openById(dokumentId);
 
-  // Pobierz tekst z dokumentu
   var tekstZDokumentu = dokument.getBody().getText();
 
-  // Użyj wyrażeń regularnych do wyodrębnienia fragmentu tekstu
   var regex = /Przysiad ze sztangą z przodu:(.*?)(Przysiad ze sztangą z tyłu \(high bar\)|$)/gs;
   var dopasowanie = tekstZDokumentu.match(regex);
 
-  // Jeśli coś zostało dopasowane, pobierz pierwsze dopasowanie
   var fragmentTekstu = (dopasowanie && dopasowanie.length > 0) ? dopasowanie[0] : "";
   fragmentTekstu = fragmentTekstu.toString().replace("Przysiad ze sztangą z tyłu (high bar)", "")
-  // ID arkusza kalkulacyjnego Google
   var arkuszId = '1RxVMwBBjImggdapV5QLu9ng_yPYhQsqyDCeCoz7OHSI';
 
-  // Otwórz arkusz kalkulacyjny
   var arkusz = SpreadsheetApp.openById(arkuszId);
 
-  // Wklej wyodrębniony fragment tekstu do wskazanej komórki w arkuszu kalkulacyjnym
   arkusz.getActiveSheet().getRange('A1').setValue(fragmentTekstu);
 }
